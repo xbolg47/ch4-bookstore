64c406c2bd2915f96d5d1b9b96a93e2cb38c5388
Xbolg <xbolg.47@gmail.com>
2026-08-24 18:52:24 +0100

    Add accounts app with custom user model and initial setup

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    # Add any additional fields you want for your custom user model here
+    pass
--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Add your local custom apps here
+    "accounts.apps.AccountsConfig",  # Add the accounts app to the installed apps
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"  # Use the custom user model
