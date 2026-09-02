6d82fee8ad7467a32c8f17410aacbab5565ea993
Xbolg <xbolg.47@gmail.com>
2026-09-02 21:16:10 +0100

    Add user authentication URLs and enhance home page with login functionality

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    # Django admin
     path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
     path("", include("pages.urls")),  # new
 ]
